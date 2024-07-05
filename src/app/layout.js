5f5015ec850475fed71b9748c09164dfae7a8f85
fhoinaski <vendasfhok@gmail.com>
2024-07-05 00:29:16 -0300

    melhorias no metatag

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
 
-
 const Navbar = dynamic(() => import('@/components/Navbar'), { ssr: false });
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,23 +18,38 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <Head>
-        <meta name="description" content="Dizem adeus aos cadastros burocráticos e descontos perdidos para sempre! Com @eficaz.email, você pode criar infinitos endereços de email personalizados e profissionais em instantes. Nossa solução inovadora desbloqueará um mundo de promoções exclusivas, ofertas imperdíveis e bônus tentadores em qualquer site. Apenas alguns cliques e seu novo email estará pronto, encaminhando discretamente todas as mensagens para sua caixa de entrada principal. Não é um serviço temporário - seu endereço @eficaz.email permanece ativo indefinidamente, mesmo no plano gratuito. Liberte-se das restrições de email e desfrute de descontos ilimitados com facilidade. Junte-se à revolução hoje!" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Diga adeus aos cadastros burocráticos e descontos perdidos para sempre! Com @eficaz.email, você pode criar infinitos endereços de email personalizados e profissionais em instantes. Nossa solução inovadora desbloqueará um mundo de promoções exclusivas, ofertas imperdíveis e bônus tentadores em qualquer site. Apenas alguns cliques e seu novo email estará pronto, encaminhando discretamente todas as mensagens para sua caixa de entrada principal. Não é um serviço temporário - seu endereço @eficaz.email permanece ativo indefinidamente, mesmo no plano gratuito. Liberte-se das restrições de email e desfrute de descontos ilimitados com facilidade. Junte-se à revolução hoje!" />
+        <meta name="keywords" content="email personalizado, email profissional, descontos ilimitados, eficaz.email, promoções exclusivas, criação de email, encaminhamento de email, plano gratuito" />
+        <meta name="author" content="Eficaz Email" />
+        <meta name="robots" content="index, follow" />
+        <meta name="og:title" content="Liberte-se das Restrições de Email com @eficaz.email: A Revolução dos Descontos Ilimitados" />
+        <meta name="og:description" content="Com @eficaz.email, você pode criar infinitos endereços de email personalizados e profissionais em instantes. Aproveite promoções exclusivas, ofertas imperdíveis e bônus tentadores em qualquer site. Seu endereço @eficaz.email permanece ativo indefinidamente, mesmo no plano gratuito." />
+        <meta name="og:type" content="website" />
+        <meta name="og:url" content="https://www.eficazmail.com" />
+        <meta name="og:image" content="https://www.eficazmail.com/images/og-image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Liberte-se das Restrições de Email com @eficaz.email: A Revolução dos Descontos Ilimitados" />
+        <meta name="twitter:description" content="Com @eficaz.email, você pode criar infinitos endereços de email personalizados e profissionais em instantes. Aproveite promoções exclusivas, ofertas imperdíveis e bônus tentadores em qualquer site. Seu endereço @eficaz.email permanece ativo indefinidamente, mesmo no plano gratuito." />
+        <meta name="twitter:image" content="https://www.eficazmail.com/images/twitter-card.jpg" />
+        <meta name="twitter:site" content="@eficaz.email" />
+        <link rel="canonical" href="https://www.eficazmail.com" />
+        <link rel="icon" href="/favicon.ico" />
         <title>Liberte-se das Restrições de Email com @eficaz.email: A Revolução dos Descontos Ilimitados</title>
       </Head>
       <body className={inter.className}>
-      
-          {!isDashboard && (
-            <header>
-              <Navbar />
-            </header>
-          )}
-          <main className='w-full'>
-            {children}
-          </main>
-          <footer>
-            <Footer />
-          </footer>
-    
+        {!isDashboard && (
+          <header>
+            <Navbar />
+          </header>
+        )}
+        <main className='w-full'>
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
       </body>
     </html>
   );
